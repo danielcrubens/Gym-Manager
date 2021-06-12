d92dd669bf0294334d721c348658183d20faf5ab
danielrubens <dcr.tecnologi@gmail.com>
2021-06-12 20:51:21 -0300

    manipulando datas :memo:

--- instructors.js
+++ instructors.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 /* const { url } = require("inspector"); */
 const data = require("./data.json");
+const {age, date} =require('./utils')
+
 
 //show
 exports.show = function(req,res){
@@ -10,22 +12,12 @@ exports.show = function(req,res){
 })
 if (!foundInstructor)return res.send("Instructor not found!")
 
-function age(timestamp){
-  const today = new Date()
-  const birthDate = new Date(timestamp)
 
-  let age = today.getFullYear() - birthDate.getFullYear()
-  const month = today.getMonth() - birthDate.getMonth()
-  if (month <0 || month == 0 && today.getDate()<= birthDate.getDate()) {
-    age = age -1
-  }
-  return age
-}
 const  instructor={
   ...foundInstructor,
   age: age(foundInstructor.birth),
   services:foundInstructor.services.split(","),
-  created_at:"",
+  created_at: new Intl.DateTimeFormat("pt-BR").format(foundInstructor.created_at),
 }
 
 return res.render("instructors/show",{instructor})
@@ -65,4 +57,21 @@ exports.post = function (req, res) {
   });
   //return res.send(req.body)
 };
+//edit
 
+exports.edit =function(req,res){
+
+  const {id} = req.params
+  const foundInstructor = data.instructors.find(function(instructor){
+  return instructor.id == id
+})
+if (!foundInstructor)return res.send("Instructor not found!")
+const instructor ={
+  ...foundInstructor,
+  birth: date(foundInstructor.birth)
+}
+
+
+  
+  return res.render('instructors/edit',{instructor})
+}
