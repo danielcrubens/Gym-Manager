ec7381275db7c3bd3725c32159b37896c13d1825
danielcrubens <dcr.tecnologi@gmail.com>
2021-08-21 23:53:29 -0300

    inserindo dados no bd :fire:

--- src/app/controllers/instructors.js
+++ src/app/controllers/instructors.js
@@ -1,14 +1,18 @@
 const { age, date } = require("../../lib/utils");
+const db = require("../../config/db");
 
 module.exports = {
-  index(req, res){
-    return res.render("instructors/index");
+  index(req, res) {
+    db.query(`SELECT *FROM instructors`,function(err,results){
+      if(err) return res.send("Database Error!");
+      return res.render("instructors/index",{instructors:results.rows});
+
+    })
   },
-  create(req, res){
+  create(req, res) {
     return res.render("instructors/create");
   },
-  post(req, res){
-    //estrutura de validação{
+  post(req, res) {
     const keys = Object.keys(req.body);
 
     for (key of keys) {
@@ -16,20 +20,41 @@ module.exports = {
         return res.send("Preencha todos os campos!");
       }
     }
-    let { avatar_url, birth, name, services, gender, type_class } = req.body;
+    const query = `
+    INSERT INTO instructors(
+      name,
+      avatar_url,
+      gender,
+      services,
+      birth,
+      created_at
+    ) VALUES ($1,$2,$3,$4,$5,$6)
+    RETURNING id
+    `;
+    const values = [
+      req.body.name,
+      req.body.avatar_url,
+      req.body.gender,
+      req.body.services,
+      date(req.body.birth).iso,
+      date(Date.now()).iso,
+    ];
 
+    db.query(query, values, function (err, results) {
+      if(err) return res.send("Database Error!");
+      return res.redirect(`/instructors/${results.rows[0].id}`);
+    });
+  },
+  show(req, res) {
     return;
   },
-  show(req, res){
+  edit(req, res) {
     return;
   },
-  edit(req, res){
+  put(req, res) {
     return;
   },
-  put(req, res){
-    return;
-  },
-  delete(req, res){
+  delete(req, res) {
     return;
   },
 };
